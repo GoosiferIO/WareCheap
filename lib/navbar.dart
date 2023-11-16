a198a20760fb73e4898145080bd170f080aae917
arose510 <98431893+arose510@users.noreply.github.com>
2023-11-16 10:40:50 -0800

    Navbar Changes/Freed View

--- lib/navbar.dart
+++ lib/navbar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:warecheap/storereview.dart';
+import 'package:warecheap/itemreview.dart';
 
 @override
 Widget navBar(context) {
@@ -6,58 +8,106 @@ Widget navBar(context) {
     child: ListView(
       padding: EdgeInsets.zero,
       children: [
-        // Add the big text at the top
         Container(
           padding: const EdgeInsets.all(16.0),
-          color: Colors.lightGreen[900], // Set the background color to blue
+          color: Colors.lightGreen[900],
           child: const Text(
-            'Warecheap', // Replace with your app's name
+            'Warecheap',
             style: TextStyle(
-              fontSize: 24.0, // Adjust the font size as needed
+              fontSize: 24.0,
               fontWeight: FontWeight.bold,
-              color: Colors.white, // Set the text color to white
+              color: Colors.white,
             ),
           ),
         ),
         const Divider(),
         ListTile(
-            leading: const Icon(Icons.account_circle),
-            title: const Text('Home'),
-            onTap: () {
-              Navigator.pushNamedAndRemoveUntil(
-                  context, '/first', (route) => false);
-            }),
+          leading: const Icon(Icons.account_circle),
+          title: const Text('Home'),
+          onTap: () {
+            Navigator.pushNamedAndRemoveUntil(
+              context,
+              '/first',
+              (route) => false,
+            );
+          },
+        ),
         const Divider(),
         ListTile(
           leading: const Icon(Icons.settings),
           title: const Text('Browse'),
           onTap: () {
             Navigator.pushNamedAndRemoveUntil(
-                context, '/second', (route) => false);
+              context,
+              '/second',
+              (route) => false,
+            );
           },
         ),
-        ListTile(
+        const Divider(),
+        // Adding ExpansionTile for 'Forum'
+        ExpansionTile(
           leading: const Icon(Icons.settings),
           title: const Text('Forum'),
-          onTap: () {
-            Navigator.pushNamedAndRemoveUntil(
-                context, '/third', (route) => false);
-          },
+          children: [
+            ListTile(
+              leading: const Icon(Icons.subdirectory_arrow_right),
+              title: const Text('Leave a review for a store'),
+              onTap: () {
+                // Navigate to Screen2 (View2)
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => reviewStore()),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.subdirectory_arrow_right),
+              title: const Text('Leave a review for an item'),
+              onTap: () {
+                // Navigate to Screen2 (View2)
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => reviewItem()),
+                );
+              },
+            ),
+          ],
         ),
+        const Divider(),
         ListTile(
           leading: const Icon(Icons.settings),
           title: const Text('Google Sign Up'),
           onTap: () {
             Navigator.pushNamedAndRemoveUntil(
-                context, '/Google', (route) => false);
+              context,
+              '/Google',
+              (route) => false,
+            );
           },
         ),
+        const Divider(),
         ListTile(
           leading: const Icon(Icons.settings),
           title: const Text('Profile Page'),
           onTap: () {
             Navigator.pushNamedAndRemoveUntil(
-                context, '/Profile', (route) => false);
+              context,
+              '/Profile',
+              (route) => false,
+            );
+          },
+        ),
+        const Divider(),
+        ListTile(
+          leading: const Icon(Icons.settings),
+          title: const Text('Authors'),
+          onTap: () {
+            Navigator.pushNamedAndRemoveUntil(
+              context,
+              '/Author',
+              (route) => false,
+            );
           },
         ),
       ],
