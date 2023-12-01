341268521f8e90f2b6b3bb2b7d46a3462710de9d
Eric Galvan <lericgalvan@outlook.com>
2023-11-30 17:15:01 -0800

    fixed bug: 'assets' in pubspec to 'assets/'

--- lib/browse.dart
+++ lib/browse.dart
@@ -6,22 +6,22 @@ class Browse extends StatelessWidget {
   const Browse({super.key});
 
   @override
-  Widget build(BuildContext context)  {
-    return wcCore.coreUI(context, 'Browse', Column(
-          children: <Widget> [
-            Row(
-              children: <Widget> [
-                Text('Browse lowest prices near you'),
-                TextField(
-                    decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter a search term',
-                  ),
+  Widget build(BuildContext context) {
+    return wcCore.coreUI(
+        context,
+        'Browse',
+        Column(
+          children: <Widget>[
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              Text('Browse lowest prices near you'),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter a search term',
                 ),
-              ]
-            )
+              ),
+            ])
           ],
         ));
-      }
-    
   }
+}
